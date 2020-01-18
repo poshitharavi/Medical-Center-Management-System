efa4996831f5c90392e0f1ce40ca358afe3290ab
poshitharavi <raviposhitha@gmail.com>
2020-01-18 23:33:45 +0530

    changes done with files

--- index.php
+++ index.php
@@ -1,1 +1,30 @@
 <?php
+include_once('view/header.php');
+?>
+
+<section class="content">
+    <div class="container-fluid">
+        <div class="row clearfix">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="card">
+                    <div class="header">
+                        <div class="block-header">
+                            <h2>Home</h2>
+                        </div>
+                    </div>
+                    <div class="body">
+                        <H3 align="center">Welcome <?php echo $_SESSION['agent_name'] ?></H3>
+<h4 align="center">To Dealer Portal</h4>
+</div>
+</div>
+</div>
+</div>
+</div>
+</section>
+
+<?php
+include_once('view/default-imports.php');
+?>
+</body>
+
+</html>
